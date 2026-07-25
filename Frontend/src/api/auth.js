0296223cd2fe17d3f767a9ef3f796e34c710abf1
P-PRIYA-ai <pragatipriya2626@gmail.com>
2026-07-25 18:44:17 +0530

    add

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -3,3 +3,7 @@ import API from "./axios";
 export const loginUser = (data) => {
   return API.post("/auth/login", data);
 };
+
+export const registerUser = (data) => {
+  return API.post("/auth/register", data);
+};
