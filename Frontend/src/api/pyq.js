9fb11bea8c2d743b902ba9599061e01ecb7a0789
Priyanshu <mimoohk23@gmail.com>
2026-07-30 16:55:42 +0530

    api connections

--- Frontend/src/api/pyq.js
+++ Frontend/src/api/pyq.js
@@ -1,35 +1,37 @@
 import API from "./axios";
 
+const BASE_URL = "/pyqs";
+
 export const getAllPyqs = async (params) => {
-  const response = await API.get("/", { params });
-  return response.data.data;
+    const { data } = await API.get(BASE_URL, { params });
+    return data.data;
 };
 
 export const getPyqById = async (id) => {
-  const response = await API.get(`/${id}`);
-  return response.data.data;
+    const { data } = await API.get(`${BASE_URL}/${id}`);
+    return data.data;
 };
 
 export const uploadPyq = async (formData) => {
-  const response = await API.post("/", formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-  return response.data.data;
+    const { data } = await API.post(BASE_URL, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return data.data;
 };
 
 export const getPendingPyqs = async () => {
-  const response = await API.get("/pending");
-  return response.data.data;
+    const { data } = await API.get(`${BASE_URL}/pending`);
+    return data.data;
 };
 
 export const approvePyq = async (id) => {
-  const response = await API.patch(`/${id}/approve`);
-  return response.data.data;
+    const { data } = await API.patch(`${BASE_URL}/${id}/approve`);
+    return data.data;
 };
 
 export const deletePyq = async (id) => {
-  const response = await API.delete(`/${id}`);
-  return response.data;
+    const { data } = await API.delete(`${BASE_URL}/${id}`);
+    return data;
 };
