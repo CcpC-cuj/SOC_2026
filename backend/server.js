58bb5c5b17684602cb2156f359db7328e6bb864b
Priyanshu <mimoohk23@gmail.com>
2026-08-16 01:45:37 +0530

    Improved Server Connection

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,24 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectDB();
-
 const PORT = process.env.PORT || 500;
 
-app.get("/", (req, res)=>{
-    res.send("Welcome to SoC");
-});
+const startServer = async()=>{
+    try{
+        await connectDB();
+    
+        app.get("/", (req, res)=>{
+            res.send("Welcome to SoC");
+        });
 
-app.listen(PORT, ()=>{
-    console.log(`Server started on ${PORT}`);
-});
+        app.listen(PORT, ()=>{
+            console.log(`Server started on ${PORT}`);
+        });
+    }
+    catch(error){
+        console.log("Server Failled!");
+        process.exit(1);
+    }
+};
+
+startServer();
